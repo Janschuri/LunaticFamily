5aa82486ccddf26f1c8423648858a3b1f571da14
Janschuri <jannis.schuricht@gmail.com>
2024-04-22 22:10:15 +0200

    code cleanup an refactoring

--- src/main/java/de/janschuri/lunaticFamily/listener/velocity/QuitListener.java
+++ src/main/java/de/janschuri/lunaticFamily/listener/velocity/QuitListener.java
@@ -1,4 +1,17 @@
 package de.janschuri.lunaticFamily.listener.velocity;
 
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import de.janschuri.lunaticFamily.listener.QuitEvent;
+import de.janschuri.lunaticFamily.senders.paper.PlayerCommandSender;
+
 public class QuitListener {
+
+    QuitEvent quitEvent = new QuitEvent();
+
+    @Subscribe
+    public void onPlayerDisconnect(DisconnectEvent event) {
+        PlayerCommandSender playerCommandSender = new PlayerCommandSender(event.getPlayer().getUniqueId());
+        quitEvent.execute(playerCommandSender);
+    }
 }
