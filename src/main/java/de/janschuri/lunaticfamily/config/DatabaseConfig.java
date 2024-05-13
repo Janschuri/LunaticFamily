4770b63aaa2da90ac7796077db16daabfbe0cd97
Janschuri <jannis.schuricht@gmail.com>
2024-05-13 21:04:29 +0200

    velocity database config

--- src/main/java/de/janschuri/lunaticfamily/config/DatabaseConfig.java
+++ src/main/java/de/janschuri/lunaticfamily/config/DatabaseConfig.java
@@ -1,6 +1,8 @@
 package de.janschuri.lunaticfamily.config;
 
+import de.janschuri.lunaticlib.LunaticLib;
 import de.janschuri.lunaticlib.config.AbstractDatabaseConfig;
+import de.janschuri.lunaticlib.utils.Platform;
 
 import java.nio.file.Path;
 
@@ -8,9 +10,10 @@ public class DatabaseConfig extends AbstractDatabaseConfig {
     private static final String NAME = "lunaticfamily";
     private static final String DATABASE_FILE = "database.yml";
     private static final String DEFAULT_DATABASE_FILE = "database.yml";
+    private static final String DATABASE_CONFIG_VELOCITY = "database-velocity.yml";
 
     public DatabaseConfig(Path dataDirectory) {
-        super(NAME, dataDirectory, DATABASE_FILE, DEFAULT_DATABASE_FILE);
+        super(NAME, dataDirectory, DATABASE_FILE, LunaticLib.getPlatform() == Platform.VELOCITY ? DATABASE_CONFIG_VELOCITY : DEFAULT_DATABASE_FILE);
         load();
     }
 }
