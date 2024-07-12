ba3190a928601642d3ef485720ca4bac3a70663d
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-07-12 21:13:26 +0200

    increased version

--- lunaticfamily-platform/lunaticfamily-platform-velocity/src/main/java/de/janschuri/lunaticfamily/platform/velocity/VelocityLunaticFamily.java
+++ lunaticfamily-platform/lunaticfamily-platform-velocity/src/main/java/de/janschuri/lunaticfamily/platform/velocity/VelocityLunaticFamily.java
@@ -21,7 +21,7 @@ import java.util.concurrent.Callable;
 @Plugin(
         id = "lunaticfamily",
         name = "LunaticFamily",
-        version = "1.2.4",
+        version = "1.2.5",
         authors = "janschuri",
         dependencies = {
         @Dependency(id = "lunaticlib")
