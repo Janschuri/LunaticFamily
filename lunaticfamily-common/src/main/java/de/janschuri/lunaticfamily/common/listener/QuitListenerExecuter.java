4e334d6ce847e153b05ec1bff359f977e5df9ed7
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-03-16 18:30:09 +0100

    german default message

--- lunaticfamily-common/src/main/java/de/janschuri/lunaticfamily/common/listener/QuitListenerExecuter.java
+++ lunaticfamily-common/src/main/java/de/janschuri/lunaticfamily/common/listener/QuitListenerExecuter.java
@@ -24,10 +24,18 @@ import static de.janschuri.lunaticfamily.common.handler.FamilyPlayer.findOrCreat
 public class QuitListenerExecuter {
 
     private static final MessageKey PLAYER_QUIT = new LunaticMessageKey("player_quit");
-    private static final CommandMessageKey MARRY_CANCEL_MK = new LunaticCommandMessageKey(new MarryDeny(), "cancel");
-    private static final CommandMessageKey ADOPT_CANCEL_MK = new LunaticCommandMessageKey(new AdoptDeny(), "cancel");
-    private static final CommandMessageKey SIBLING_CANCEL_MK = new LunaticCommandMessageKey(new SiblingDeny(), "cancel");
-    private static final CommandMessageKey MARRY_PARTNER_LEFT_MK = new LunaticCommandMessageKey(new Marry(), "partner_left");
+    private static final CommandMessageKey MARRY_CANCEL_MK = new LunaticCommandMessageKey(new MarryDeny(), "cancel")
+            .defaultMessage("en", "The marriage proposal has been canceled.")
+            .defaultMessage("de", "Die Heiratsanfrage wurde abgebrochen.");
+    private static final CommandMessageKey ADOPT_CANCEL_MK = new LunaticCommandMessageKey(new AdoptDeny(), "cancel")
+            .defaultMessage("en", "The adoption proposal has been canceled.")
+            .defaultMessage("de", "Die Adoptionsanfrage wurde abgebrochen.");
+    private static final CommandMessageKey SIBLING_CANCEL_MK = new LunaticCommandMessageKey(new SiblingDeny(), "cancel")
+            .defaultMessage("en", "The sibling proposal has been canceled.")
+            .defaultMessage("de", "Die Geschwisteranfrage wurde abgebrochen.");
+    private static final CommandMessageKey MARRY_PARTNER_LEFT_MK = new LunaticCommandMessageKey(new Marry(), "partner_left")
+            .defaultMessage("en", "Your partner has left the server.")
+            .defaultMessage("de", "Dein Partner hat den Server verlassen.");
 
 
     public static boolean execute(PlayerSender player) {
